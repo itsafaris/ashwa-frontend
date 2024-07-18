85dabd2c9eeba2acf82faebb90b9cd75ed125813
Vilius Linge <viliusling@gmail.com>
2024-07-18 17:26:55 +0300

    Update shades

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Logo } from "./logo";
 import { Link } from "gatsby";
 
 export const Header = () => (
-  <Box bg="shade.200">
+  <Box bg="shade.100">
     <Container as={Flex} py={3} alignItems={"center"} justifyContent={"center"}>
       <Link to="/">
         <Logo height={"24px"} />
