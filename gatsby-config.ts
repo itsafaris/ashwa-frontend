fda981bdc1fd742bc82e2a729afb9f39348851de
martynasj <martyjankauskas@gmail.com>
2024-07-19 10:26:56 +0300

    merge confg with sitemeta

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,21 +1,10 @@
 import type { GatsbyConfig } from "gatsby";
-import pkgjson from "./package.json";
-import { SiteMetadata } from "./src/hooks/useSiteMetadata";
 
 const config: GatsbyConfig = {
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  siteMetadata: {
-    brandName: "Calmr",
-    siteUrl: `https://www.trycalmr.com`,
-    version: pkgjson.version,
-    image: "/images/meta-image.jpg",
-    title: `Most complete Blend of Ashwagandha for Modern Busy Women`,
-    description:
-      "Our all-in-one supplement blend - the only one you'll need to combat stress, sharpen focus, and boost energy. This revolutionary formula uniquely combines Ashwagandha, Rhodiola Extract, and Bacopa with essential vitamins and minerals, creating a powerhouse solution tailored specifically for the modern woman's needs.",
-  } satisfies SiteMetadata,
   plugins: [
     `gatsby-plugin-tsconfig-paths`,
     "gatsby-plugin-emotion",
---
+++ src/pages/purchase-success.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export interface IPurchaseSuccessPageProps {}
+
+export function PurchaseSuccessPage(props: IPurchaseSuccessPageProps) {
+  return <div>thank you</div>;
+}
