5e38d39e8127fb0543f6a187ab28784e66aa111c
Vilius Linge <viliusling@gmail.com>
2024-07-26 14:47:50 +0300

    Update texts

--- src/pages/weight-loss/index.tsx
+++ src/pages/weight-loss/index.tsx
@@ -25,51 +25,56 @@ export default function WeightLossPage(props: IWeightLossPageProps) {
             Weight Keeps Coming Back?
           </Heading>
 
-          <Box bg="yellow.200" p={2} borderRadius={32}>
+          <Stack mb={4}>
+            <CTA />
+          </Stack>
+
+          <Stack bg="shade.200" p={4} borderRadius={20}>
+            <Text
+              fontStyle={"italic"}
+              fontWeight={"bold"}
+              fontSize={"xl"}
+              textAlign={"center"}
+              color={"shade.800"}
+              pb={3}
+            >
+              "Hey, I’m Olivia, and I’ve been using Calmr for 5 months!"
+            </Text>
+
             <StaticImage
               src="../../images/landing-hero-img.jpg"
               alt="women using ashwagandha supplements"
               style={{
-                borderRadius: 24,
+                borderRadius: 10,
               }}
             />
-          </Box>
 
-          <Stack alignItems={"center"}>
-            <Text fontWeight={"semibold"}>Take a 1-min quiz to find the reason why</Text>
+            <Stack spacing={4} pt={3} fontSize={"sm"} color="shade.800" fontStyle={"italic"}>
+              <Text>
+                Stress was a big part of my life, and no matter what I did, my cortisol levels
+                stayed high. I started using Calmr about five months ago, and slowly, my stress
+                began to ease.
+              </Text>
+
+              <Text>
+                More surprisingly,{" "}
+                <Text as="span" fontWeight={"bold"} textDecor={"underline"} color={"shade.900"}>
+                  I've lost 22 pounds without any major diet changes
+                </Text>{" "}
+                and feel lighter, both physically and mentally. My stomach issues have also eased
+                up, making daily activities more comfortable.
+              </Text>
+
+              <Text>
+                Although I'm still on this journey, I'm curious to see what the final results will
+                be.
+              </Text>
+            </Stack>
           </Stack>
 
-          <Flex direction={"column"} gap={4}>
-            <Flex gap={3} width={"100%"} px={4}>
-              <Button
-                borderRadius={"full"}
-                as={Link}
-                size={"lg"}
-                flex={1}
-                colorScheme="teal"
-                to={`quiz?${createPageParams({
-                  resetState: true,
-                  gender: "male",
-                }).toString()}`}
-              >
-                Male
-              </Button>
-
-              <Button
-                borderRadius={"full"}
-                as={Link}
-                flex={1}
-                size={"lg"}
-                colorScheme="primary"
-                to={`quiz?${createPageParams({
-                  resetState: true,
-                  gender: "female",
-                }).toString()}`}
-              >
-                Female
-              </Button>
-            </Flex>
-          </Flex>
+          <Stack my={4}>
+            <CTA text="Take a 1-min quiz to find the reason why your weight keeps comming back" />
+          </Stack>
 
           <Stack spacing={2} alignItems={"center"} my={6}>
             <ListItem text={"Find the reason of your weight troubles"} />
@@ -135,5 +140,45 @@ function ListItem({ text, ...rest }: { text: string } & ComponentProps<typeof Fl
         {text}
       </Text>
     </Flex>
+  );
+}
+
+function CTA({ text = "Take a 1-min quiz to find the reason why" }: { text?: string }) {
+  return (
+    <Stack alignItems={"center"} spacing={4}>
+      <Text textAlign={"center"} fontWeight={"semibold"}>
+        {text}
+      </Text>
+
+      <Flex gap={3} width={"100%"} px={4}>
+        <Button
+          borderRadius={"full"}
+          as={Link}
+          size={"lg"}
+          flex={1}
+          colorScheme="teal"
+          to={`quiz?${createPageParams({
+            resetState: true,
+            gender: "male",
+          }).toString()}`}
+        >
+          Male
+        </Button>
+
+        <Button
+          borderRadius={"full"}
+          as={Link}
+          flex={1}
+          size={"lg"}
+          colorScheme="primary"
+          to={`quiz?${createPageParams({
+            resetState: true,
+            gender: "female",
+          }).toString()}`}
+        >
+          Female
+        </Button>
+      </Flex>
+    </Stack>
   );
 }
