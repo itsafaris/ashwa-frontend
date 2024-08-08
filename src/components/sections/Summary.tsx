4c03607bdeaf0906de17a443cae69e3fd42ffb34
Vilius Linge <viliusling@gmail.com>
2024-08-08 20:04:02 +0300

    Update path

--- src/components/sections/Summary.tsx
+++ src/components/sections/Summary.tsx
@@ -10,7 +10,7 @@ import { GiTreeBranch } from "react-icons/gi";
 import { Box, Heading, Text, Icon, Container, Stack, Grid } from "@chakra-ui/react";
 
 import { CTAButton } from "../components";
-import { useReadableState } from "../../pages/summary/ctx";
+import { useReadableState } from "../../pages/summary";
 import { StaticImage } from "gatsby-plugin-image";
 
 export const Summary = () => {
