e91002706bce422b49277bc4cde718268c623332
Vilius Linge <viliusling@gmail.com>
2024-08-08 13:55:00 +0300

    Update faq section

--- src/pages/summary/FAQ.tsx
+++ src/pages/summary/FAQ.tsx
@@ -3,7 +3,7 @@ import { FAQ } from "@components/FAQ";
 
 export function FAQSection() {
   return (
-    <Box py={8}>
+    <Box py={8} bg="primary.100">
       <Container maxW={"container.md"}>
         <FAQ />
       </Container>
