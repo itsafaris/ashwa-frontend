066250c45d07864ca8d6ade99afdc21ccb0b0906
Vilius Linge <viliusling@gmail.com>
2024-08-09 11:32:38 +0300

    Update landing page ui

--- src/pages/weight-loss/index.tsx
+++ src/pages/weight-loss/index.tsx
@@ -30,28 +30,28 @@ export default function WeightLossPage(props: IWeightLossPageProps) {
             <CTA />
           </Stack>
 
-          <Stack bg="primary.300" p={4} borderRadius={20}>
-            <Text
-              fontStyle={"italic"}
-              fontWeight={"bold"}
-              fontSize={"xl"}
-              textAlign={"center"}
-              color={"gray.900"}
-              pb={4}
-              pt={2}
-            >
-              "Hey, I'm Olivia, and I've been using Calmr for 5 months!"
-            </Text>
+          <Stack p={4} backgroundColor={"white"}>
+            <Stack pt={1} pb={4}>
+              <Text fontSize={"xs"} color="gray.700">
+                17 July, 2024 | 1 MIN READ
+              </Text>
+
+              <Text mt={1} fontWeight={"bold"} fontSize={"xl"} color={"gray.900"} lineHeight={1.3}>
+                Hey, I'm Olivia, and I've been using Calmr for 5 months!
+              </Text>
+
+              <Text fontSize={"sm"} color="gray.700">
+                Stress was a big part of my life, and no matter what I did, my cortisol levels
+                stayed high.
+              </Text>
+            </Stack>
 
             <StaticImage
               src="../../images/customer-before-after-1.jpg"
               alt="women using ashwagandha supplements"
-              style={{
-                borderRadius: 10,
-              }}
             />
 
-            <Stack spacing={4} pt={3} fontSize={"md"} color="gray.900" fontStyle={"italic"}>
+            <Stack spacing={4} pt={3} fontSize={"sm"} color="gray.700">
               <Text>
                 Stress was a big part of my life, and no matter what I did, my cortisol levels
                 stayed high. I started using Calmr about five months ago, and slowly, my stress
@@ -60,7 +60,13 @@ export default function WeightLossPage(props: IWeightLossPageProps) {
 
               <Text>
                 More surprisingly,{" "}
-                <Text as="span" fontWeight={"bold"} textDecor={"underline"} color={"gray.900"}>
+                <Text
+                  backgroundColor={"primary.200"}
+                  as="span"
+                  fontWeight={"bold"}
+                  textDecor={"underline"}
+                  color={"gray.900"}
+                >
                   I've lost 22 pounds without any major diet changes
                 </Text>{" "}
                 and feel lighter, both physically and mentally. My stomach issues have also eased
@@ -72,55 +78,61 @@ export default function WeightLossPage(props: IWeightLossPageProps) {
                 be.
               </Text>
             </Stack>
+
+            <Stack spacing={10} mt={12}>
+              <Text
+                mx="auto"
+                maxW={"200px"}
+                textAlign={"center"}
+                fontWeight={"bold"}
+                fontSize={"sm"}
+                color="gray.900"
+              >
+                Benefits of Calmr ingredients are covered in
+              </Text>
+
+              <Stack pb={4}>
+                <Flex
+                  wrap={"wrap"}
+                  gap={[6, 10]}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  opacity={0.4}
+                >
+                  <StaticImage
+                    style={{ flexShrink: 0 }}
+                    height={28}
+                    src="../../images/media1.png"
+                    alt="media logo"
+                  />
+
+                  <StaticImage
+                    style={{ flexShrink: 0 }}
+                    height={28}
+                    src="../../images/media2.png"
+                    alt="media logo"
+                  />
+
+                  <StaticImage
+                    style={{ flexShrink: 0 }}
+                    height={46}
+                    src="../../images/media3.png"
+                    alt="media logo"
+                  />
+
+                  <StaticImage
+                    style={{ flexShrink: 0 }}
+                    height={52}
+                    src="../../images/media4.png"
+                    alt="media logo"
+                  />
+                </Flex>
+              </Stack>
+            </Stack>
           </Stack>
 
           <Stack mt={4} mb={6}>
             <CTA text="Take a 1-min quiz to get our recommendations" />
-          </Stack>
-
-          <Stack spacing={3}>
-            <Text
-              mx="auto"
-              maxW={"200px"}
-              textAlign={"center"}
-              fontWeight={"semibold"}
-              fontSize={"sm"}
-              color="gray.700"
-            >
-              Benefits of Calmr ingredients are covered in:
-            </Text>
-
-            <Stack mb={6} backgroundColor={"white"} p={7} borderRadius={"lg"}>
-              <Flex wrap={"wrap"} gap={[6, 10]} justifyContent={"center"} alignItems={"center"}>
-                <StaticImage
-                  style={{ flexShrink: 0 }}
-                  height={28}
-                  src="../../images/media1.png"
-                  alt="media logo"
-                />
-
-                <StaticImage
-                  style={{ flexShrink: 0 }}
-                  height={28}
-                  src="../../images/media2.png"
-                  alt="media logo"
-                />
-
-                <StaticImage
-                  style={{ flexShrink: 0 }}
-                  height={46}
-                  src="../../images/media3.png"
-                  alt="media logo"
-                />
-
-                <StaticImage
-                  style={{ flexShrink: 0 }}
-                  height={52}
-                  src="../../images/media4.png"
-                  alt="media logo"
-                />
-              </Flex>
-            </Stack>
           </Stack>
         </Stack>
       </Container>
