330d1866cfe290150032bfc7719cb3395d457f39
Vilius Linge <viliusling@gmail.com>
2024-08-09 13:49:10 +0300

    Update landing page

--- src/pages/weight-loss/index.tsx
+++ src/pages/weight-loss/index.tsx
@@ -22,22 +22,31 @@ export default function WeightLossPage(props: IWeightLossPageProps) {
 
       <Container my={4} py={6}>
         <Stack spacing={4}>
-          <Heading fontSize={"3xl"} textAlign={"center"} fontWeight={"semibold"}>
+          <Heading
+            lineHeight={1.2}
+            fontSize={"3xl"}
+            textAlign={"center"}
+            fontWeight={"bold"}
+            color="primary.800"
+          >
             Is the high cortisol level the main cause of your weight gain?
           </Heading>
 
-          <Stack mt={4} mb={6}>
+          <Stack mt={4} mb={6} alignItems={"center"} spacing={4}>
+            <Text textAlign={"center"} fontWeight={"semibold"}>
+              Take a 1-min quiz to see your results
+            </Text>
+
             <CTA />
           </Stack>
 
-          <Stack p={4} backgroundColor={"white"}>
-            <Stack pt={1} pb={4}>
-              <Text fontSize={"xs"} color="gray.700">
-                17 July, 2024 | 1 MIN READ
+          <Stack p={4} backgroundColor={"white"} boxShadow={"lg"}>
+            <Stack pt={1} pb={2}>
+              <Text fontWeight={"semibold"} fontSize={"xl"} color={"gray.900"} lineHeight={1.3}>
+                Hey, I'm Jessica, and I've been using Calmr for 5 months!
               </Text>
-
-              <Text mt={1} fontWeight={"bold"} fontSize={"xl"} color={"gray.900"} lineHeight={1.3}>
-                Hey, I'm Olivia, and I've been using Calmr for 5 months!
+              <Text fontSize={10} color="gray.600" mb={3}>
+                17 July, 2024 | 1 MIN READ
               </Text>
 
               <Text fontSize={"sm"} color="gray.700">
@@ -51,11 +60,9 @@ export default function WeightLossPage(props: IWeightLossPageProps) {
               alt="women using ashwagandha supplements"
             />
 
-            <Stack spacing={4} pt={3} fontSize={"sm"} color="gray.700">
+            <Stack spacing={4} pt={2} fontSize={"sm"} color="gray.700">
               <Text>
-                Stress was a big part of my life, and no matter what I did, my cortisol levels
-                stayed high. I started using Calmr about five months ago, and slowly, my stress
-                began to ease.
+                I started using Calmr about five months ago, and slowly, my stress began to ease.
               </Text>
 
               <Text>
@@ -67,7 +74,7 @@ export default function WeightLossPage(props: IWeightLossPageProps) {
                   textDecor={"underline"}
                   color={"gray.900"}
                 >
-                  I've lost 22 pounds without any major diet changes
+                  I've lost 29 pounds without any major diet changes
                 </Text>{" "}
                 and feel lighter, both physically and mentally. My stomach issues have also eased
                 up, making daily activities more comfortable.
@@ -131,8 +138,18 @@ export default function WeightLossPage(props: IWeightLossPageProps) {
             </Stack>
           </Stack>
 
-          <Stack mt={4} mb={6}>
-            <CTA text="Take a 1-min quiz to get our recommendations" />
+          <Stack mt={8} mb={8} alignItems={"center"} spacing={4}>
+            <Heading
+              lineHeight={1.2}
+              fontSize={"2xl"}
+              textAlign={"center"}
+              fontWeight={"bold"}
+              color="primary.800"
+            >
+              Take a 1-min quiz to see your results
+            </Heading>
+
+            <CTA />
           </Stack>
         </Stack>
       </Container>
@@ -142,61 +159,44 @@ export default function WeightLossPage(props: IWeightLossPageProps) {
   );
 }
 
-function ListItem({ text, ...rest }: { text: string } & ComponentProps<typeof Flex>) {
+function CTA() {
   return (
-    <Flex gap={2} alignItems={"center"} {...rest}>
-      <Icon as={CheckIcon} color="green" />
-      <Text fontSize={"sm"} fontWeight={"semibold"}>
-        {text}
-      </Text>
+    <Flex gap={2} width={"100%"}>
+      <Button
+        borderRadius={"full"}
+        as={Link}
+        size={"lg"}
+        py={7}
+        flex={1}
+        colorScheme="teal"
+        to={`quiz?${createPageParams({
+          resetState: true,
+          gender: "male",
+        }).toString()}`}
+      >
+        <Stack direction={"row"} alignItems={"center"} spacing={2}>
+          <Text lineHeight={1}>Male</Text>
+          <Icon as={FaArrowRight} />
+        </Stack>
+      </Button>
+
+      <Button
+        borderRadius={"full"}
+        as={Link}
+        flex={1}
+        size={"lg"}
+        py={7}
+        colorScheme="pink"
+        to={`quiz?${createPageParams({
+          resetState: true,
+          gender: "female",
+        }).toString()}`}
+      >
+        <Stack direction={"row"} alignItems={"center"} spacing={2}>
+          <Text lineHeight={1}>Female</Text>
+          <Icon as={FaArrowRight} />
+        </Stack>
+      </Button>
     </Flex>
-  );
-}
-
-function CTA({ text = "Take a 1-min quiz to find out" }: { text?: string }) {
-  return (
-    <Stack alignItems={"center"} spacing={4}>
-      <Text textAlign={"center"} fontWeight={"semibold"}>
-        {text}
-      </Text>
-
-      <Flex gap={2} width={"100%"}>
-        <Button
-          borderRadius={"full"}
-          as={Link}
-          size={"lg"}
-          py={7}
-          flex={1}
-          colorScheme="teal"
-          to={`quiz?${createPageParams({
-            resetState: true,
-            gender: "male",
-          }).toString()}`}
-        >
-          <Stack direction={"row"} alignItems={"center"} spacing={2}>
-            <Text lineHeight={1}>Male</Text>
-            <Icon as={FaArrowRight} />
-          </Stack>
-        </Button>
-
-        <Button
-          borderRadius={"full"}
-          as={Link}
-          flex={1}
-          size={"lg"}
-          py={7}
-          colorScheme="pink"
-          to={`quiz?${createPageParams({
-            resetState: true,
-            gender: "female",
-          }).toString()}`}
-        >
-          <Stack direction={"row"} alignItems={"center"} spacing={2}>
-            <Text lineHeight={1}>Female</Text>
-            <Icon as={FaArrowRight} />
-          </Stack>
-        </Button>
-      </Flex>
-    </Stack>
   );
 }
